c882f8b308b27f082d5bf07c7b4d22385c5e4a7b
Rushali Sarkar <rushalisarkar2000@gmail.com>
2021-06-13 23:04:40 +0530

    Changed the Day 6 files

--- Day 6/task1.js
+++ Day 6/task1.js
@@ -1,9 +1,9 @@
-var person = {firstname: "Geeta", lastname: "Roy", class: "XII", roll: "36"};
+var student =  { name : "David Rayy", class : "VI", rollno : 12  };
 let keys = [];
 let values = [];
-for (var key in person) {
+for (var key in student) {
 	keys.push(key);
-	values.push(person[key]);
+	values.push(student[key]);
 }
 document.write(keys + "<br>");
 document.write(values);
